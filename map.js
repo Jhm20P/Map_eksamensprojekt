c83e6f891949f445377bfec5e8686f757718942a
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-05-20 13:13:26 +0200

    Add files via upload

---
+++ map.js
@@ -0,0 +1,27 @@
+document.querySelector(".punkt1").addEventListener("click", function tooltip() {
+  document.querySelector("#tooltip1").classList.toggle("vis-ikke");
+});
+
+document.querySelector(".punkt2").addEventListener("click", function tooltip() {
+  document.querySelector("#tooltip2").classList.toggle("vis-ikke");
+});
+
+document.querySelector(".punkt3").addEventListener("click", function tooltip() {
+  document.querySelector("#tooltip3").classList.toggle("vis-ikke");
+});
+
+document.querySelector(".punkt4").addEventListener("click", function tooltip() {
+  document.querySelector("#tooltip4").classList.toggle("vis-ikke");
+});
+
+document.querySelector(".punkt5").addEventListener("click", function tooltip() {
+  document.querySelector("#tooltip5").classList.toggle("vis-ikke");
+});
+
+document.querySelector(".punkt6").addEventListener("click", function tooltip() {
+  document.querySelector("#tooltip6").classList.toggle("vis-ikke");
+});
+
+document.querySelector(".punkt7").addEventListener("click", function tooltip() {
+  document.querySelector("#tooltip7").classList.toggle("vis-ikke");
+});
